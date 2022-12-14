89fe61da48d8db40523984dcb768596a9be1f9ef
NoelTW <as11111satw@gmail.com>
2022-12-15 00:02:27 +0800

    update data

--- object_detection/utils/all_path.py
+++ object_detection/utils/all_path.py
@@ -7,7 +7,7 @@ from dataclasses import field
 class AllPath:
     """Class for keeping track of an item in inventory."""
     _DATA_BASE_DIR:pathlib.Path = pathlib.Path('raw_data')
-    _DATA_ANOTATIONs:pathlib.Path  = _DATA_BASE_DIR / 'Annotations' / 'Annotations'
+    _DATA_ANOTATIONs:pathlib.Path = _DATA_BASE_DIR / 'Annotations' / 'Annotations'
     DATA_ANOTATIONs_DAY_TRAIN:pathlib.Path = _DATA_ANOTATIONs / 'dayTrain'
     DATA_ANOTATIONs_NIGHT_TRAIN:pathlib.Path = _DATA_ANOTATIONs / 'nightTrain'
     DATA_FILE_LOCATION_DAY_TRAIN:pathlib.Path = _DATA_BASE_DIR / 'dayTrain' / 'dayTrain'
@@ -16,4 +16,5 @@ class AllPath:
 
 
 if __name__ == 'main':
-    print(AllPath())
+    all_ =  AllPath()
+    print(all_)
